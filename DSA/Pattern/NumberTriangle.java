f4fd11ef7420c709a4a1b53340f34da84109e3d6
Santhosh <santhoshsivasankara@gmail.com>
2026-07-25 11:00:26 +0530

    updated no. triangle

--- DSA/Pattern/NumberTriangle.java
+++ DSA/Pattern/NumberTriangle.java
@@ -7,15 +7,48 @@ public class NumberTriangle {
 // 4 8 11 13
 // 5 9 12 14 15
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         for (int i = 1; i <= n; i++) {
             int counter = i;
             for (int j = 1; j <= i; j++) {
-                System.out.print(counter + " ");
+                System.out.print(counter+ " ");
                 counter = counter + (n - j);
 
             }
             System.out.println("");
         }
+
+
+        // reversePattern(5);
     }
+
+
+
+
+    static void reversePattern(int n){
+        int val = 1;
+        for(int i=0;i<n;i++){
+
+            for(int k = 0;k<n-i;k++){
+                System.out.print("   ");
+            }
+
+            int counter = val;
+
+            int step = i;
+
+            for(int j=0;j<=i;j++){
+                System.out.printf("%3d" , counter);
+                counter-=(n-step);
+                step--;
+                
+            }
+
+            System.out.println("");
+            val+= (n-(i));
+
+        }
+    }
+
 }
+
