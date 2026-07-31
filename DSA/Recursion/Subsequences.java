b9a7f304e760ed3d880ad90e288d388a0984a046
Santhosh <santhoshsivasankara@gmail.com>
2026-07-31 10:47:59 +0530

    subseq added

---
+++ DSA/Recursion/Subsequences.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Subsequences {
+
+    static void subseq(String p , String up){
+
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subseq(p+ch , up.substring(1));
+        subseq(p , up.substring(1));
+    }
+
+    static ArrayList<String> subseqArrayList(String p , String up ){
+
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = subseqArrayList(p+ch , up.substring(1));
+        ArrayList<String> right = subseqArrayList(p , up.substring(1));
+
+        left.addAll(right);
+        return left ;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        subseq("" , "abc");
+
+        ArrayList<String> subset = subseqArrayList("" , "abc");
+        System.out.println(subset);
+    }
+
+}
---
+++ System Design/Lab Booking System/src/Lab.java
@@ -0,0 +1,8 @@
+public class Lab {
+    private int labId;
+    private Student student;
+
+    public int getLabId() {
+        return labId;
+    }
+}
